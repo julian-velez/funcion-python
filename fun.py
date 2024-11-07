d32c8b58980c10522a49ff6f4f562e3d21deafe6
julian <juliansolo7189@gmail.com>
2024-11-07 15:29:52 -0500

    fun python

--- fun.py
+++ fun.py
@@ -1,6 +1,14 @@
 def multi():
-     valor1=20
-     valor2=40
-     print(valor1 * valor2)
-
+    # Se define una función llamada 'multi'. Las funciones en Python se definen con la palabra clave 'def' seguida del nombre de la función y paréntesis.
+    
+    valor1 = 20
+    # Dentro de la función, se declara una variable llamada 'valor1' y se le asigna el valor de 20.
+    
+    valor2 = 40
+    # Se declara otra variable llamada 'valor2' y se le asigna el valor de 40.
+    
+    print(valor1 * valor2)
+    # Se imprime el resultado de multiplicar 'valor1' por 'valor2'. En este caso, se realiza la multiplicación 20 * 40, que da como resultado 800.
+    
 multi()
+# Finalmente, se llama a la función 'multi()' para que se ejecute el bloque de código dentro de la función. Cuando se llama, se ejecuta la multiplicación y se imprime el resultado.
